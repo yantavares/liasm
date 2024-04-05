cd215abd8b6ab10d31efb8d4323e8527cd3b561c
yantavares <yantdo1@gmail.com>
2024-04-05 16:53:51 -0300

    Refactor liasm/tests/test3.asm to fix indentation and add comments

--- liasm/tests/test3.asm
+++ liasm/tests/test3.asm
@@ -1,5 +1,5 @@
-LOAD   N1      ; Lê o primeiro número
-LOAD   N2      ; Lê o segundo número
+LOAD   N1       ; Lê o primeiro número
+LOAD   N2       ; Lê o segundo número
 LOAD    N1      ; Carrega N1 no acumulador
 ADD     N2      ; Soma está no acumulador
 STORE   N3      ; Coloca soma N1+N2 em N3
@@ -14,5 +14,5 @@ OK:     STOP            ; Termina a execução do programa
 N1:     CONST 20   
 N2:     CONST -64
 FINAL:  CONST -44
-N3:     SPACE   ; Reserva espaço para o resultado
+N3:     SPACE            ; Reserva espaço para o resultado
 
