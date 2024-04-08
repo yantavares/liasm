d120273a74167a73eb92c08635d235b6f170b6b9
yantavares <yantdo1@gmail.com>
2024-04-08 14:35:57 -0300

    Fix formatting issues in error1.asm

--- liasm/tests/error1.asm
+++ liasm/tests/error1.asm
@@ -2,18 +2,18 @@
        
        LOAD    N
 FAT:   SUB     ONE
-       JMPZ   FIM
+       JMPZ    FIM
        STORE   AUX
-       MUL    N
+       MUL     N
        STORE   N
        LOAD    AUX
-       JMP    FAT
-FIM:   LOAD N
-       SUB FINAL
-       JMPZ  OK
+       JMP     FAT
+FIM:   LOAD    N
+       SUB     FINAL
+       JMPZ    OK
        THROW
 OK:    STOP
 AUX:   SPACE
-N:     CONST  5
-FINAL: CONST 120
+N:     CONST   5
+FINAL: CONST   120
 ; ONE:   CONST   1
